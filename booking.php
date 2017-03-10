1c85da1add9a2ff5ef171bdda6573a5ce99fdefc
karmi214 <karmi214@gmail.com>
2017-03-10 15:37:20 +0545

    search ra db
    search ra db

--- booking.php
+++ booking.php
@@ -5,24 +5,25 @@ include("navig.php") ;?>
 <div class="container-fluid bg-color ">
 	<div class="container banner-content " >
 	 <h1 id="welcome-text"><center style="margin-top:100px; margin-bottom:25px;">Find And Book Appointments</center></h1> </br>
-		<form action="" method="POST" role="form">
+		<form action="search.php" method="GET" role="form">
 			<div class="col-sm-4 ">
 				<div class="select">
-					<select name="" id="input" class="form-control specialist " required="required" title="specialist is required">
-						<option > Select Specialist</option>
+					<select name="field" id="input" class="form-control specialist " required="required" title="specialist is required" >
+						<option value=""> Select Specialist</option>
+						<option value='all'>All</option>
 						<optgroup label="Top Specialist">
-						<option value="1">Cardiologist</option>
-						<option value="2">Nureosurgeon</option>
-						<option value="3">Orthopedician</option>
-						<option value="4">Oncologist</option>
-						<option value="5">Dentist</option>              	
+						<option value="Cardiologist">Cardiologist</option>
+						<option value="Nureosurgeon">Nureosurgeon</option>
+						<option value="Orthopedician">Orthopedician</option>
+						<option value="Oncologist">Oncologist</option>
+						<option value="Dentist">Dentist</option>              	
 						</optgroup>
 						<optgroup label="Other Specialist"><hr>
-						<option value="1">Nureologist</option>
-						<option value="2">Psychiatrist</option>
-						<option value="3">Pediatrician</option>
-						<option value="4">Allergist</option>
-						<option value="5">Anesthesiologist</option>              	
+						<option value="Nureologist">Nureologist</option>
+						<option value="Psychiatrist">Psychiatrist</option>
+						<option value="Pediatrician">Pediatrician</option>
+						<option value="Allergist">Allergist</option>
+						<option value="Anesthesiologist">Anesthesiologist</option>              	
 						</optgroup>
 					</select>
 				</div>
@@ -30,12 +31,11 @@ include("navig.php") ;?>
 
 			<div class="col-sm-7">
 				<div class="select">
-				   <input type="text" name="" id="input" class="form-control  specialist " placeholder="Enter The Doctor Name" required="required" title="Dr.name is required">
+				   <input type="text" name="key" id="input" class="form-control  specialist " placeholder="Enter The Doctor Name" title="Dr.name is required">
 				</div>
 			</div>
 			<div class="col-sm-1">
-
-				<button type="submit" class="select btn-block" style=" background-color: #06a5e0;"><img src="images/search-icon.png" width="46px" height="46px" /></button>
+				<button type="submit" class="select btn-block" name="submit" style=" background-color: #06a5e0;"><img src="images/search-icon.png" width="46px" height="46px" /></button>
 
 			</div>
 
