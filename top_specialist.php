6e5e28e7a5ee608ae6fcc476e7f30d9e7dca25f3
sajan basnet <sajanbasnet75@gmail.com>
2017-03-11 14:27:34 +0545

    front end

---
+++ top_specialist.php
@@ -0,0 +1,41 @@
+<div class="container-fluid top-specialists">
+<div class="container">
+<h2 class="text-center">TOP SPECIALISTS</h2>
+<div class="row">
+<div class="col-xs-6 col-md-3 col-la-3">
+<a href="#"><img src="images/PSY.png" class="img-responsive spec-icons" alt="psychiatrist">
+</a>
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/cardio.png" class="img-responsive spec-icons" alt="cardiologist">
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/ONCO.png" class="img-responsive spec-icons" alt="oncologist">
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/PED.png" class="img-responsive spec-icons" alt="pedistrian">
+</div>
+</div>
+
+<div class="row">
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/ORTHO.png" class="img-responsive spec-icons" alt="orthologist">
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/gas.png" class="img-responsive spec-icons" alt="gasterology">
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/DENTIST.png" class="img-responsive spec-icons" alt="dentist">
+</div>
+
+<div class="col-xs-6 col-md-3 col-la-3">
+<img src="images/neurology.png" class="img-responsive spec-icons" alt="neurologist">
+</div>
+</div>
+</div>
+</div>
